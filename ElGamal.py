7f170e6912b77c202fe7ca54e57a90fe40427752
Jan Riedel <jan.riedel@everbase.net>
2015-05-10 23:01:02 +0200

    Einige fixes, debugs -> Cryptosalat? Decrypt wirft Fehler.

--- ElGamal.py
+++ ElGamal.py
@@ -1,3 +1,4 @@
+import sys
 import math
 from decimal import Decimal
 import decimal
@@ -6,8 +7,8 @@ import KeyGen as KeyGen
 import UTF8_Convert as UTF8
 import json
 decimal.getcontext().prec = 1000 # es wird alles benoetigte improtiert und die Praezision der Decimals festgestellt, welche nicht zu gering sein sollte.
-"""Curve25519: y^2=x^3+486662x^2+x
-	Prinmzahl: 2^255- 19"""
+#Curve25519: y^2=x^3+486662x^2+x
+#	Prinmzahl: 2^255- 19
 a = 486662 #Die Montomerykurve wird mit Primzahl definiert
 b = 1
 prim = Decimal(2**255 - 19)
@@ -27,9 +28,9 @@ def additionMG(p,q): #Die Addition von zwei Punkten auf der Kurve
 		y2 = Decimal(q[1]) #auslesen der Koordinaten der Punkte
 		Xr = Decimal(b*(x2*y1-x1*y2)**2/(x1*x2*(x2-x1)**2))
 		Yr = Decimal((((2*x1 + x2 + a)*(y2 - y1)) / (x2 - x1)) - b*((y2 - y1)**3 / (x2 - x1)**3) - y1)
-		"""Die Formeln erschliesst sich mit einigem aufwand aus der geometrischen Definition
-		der Addition. Dazu kann ich diese Seite empfehlen: http://en.wikipedia.org/wiki/Montgomery_curve. Etwas ungenauer findet sich das auch in 
-		theoretischen Ausarbeitung"""  
+		#Die Formeln erschliesst sich mit einigem aufwand aus der geometrischen Definition
+		#der Addition. Dazu kann ich diese Seite empfehlen: http://en.wikipedia.org/wiki/Montgomery_curve. Etwas ungenauer findet sich das auch in 
+		#theoretischen Ausarbeitung
 		r.append(Xr)
 		r.append(Yr)
 		return r
@@ -48,8 +49,8 @@ def additionMG(p,q): #Die Addition von zwei Punkten auf der Kurve
 		r.append(Xr)
 		r.append(Yr)
 		return r
-"""Im eigentlichen gibt es den Operator der multiplikation auf Elliptischen Kurven nicht, deswegen wird hier
-rekrusiv das "doubling and add" - verfahren genutzt."""
+	#Im eigentlichen gibt es den Operator der multiplikation auf Elliptischen Kurven nicht, deswegen wird hier
+	#rekrusiv das "doubling and add" - verfahren genutzt.
 def multiplikation(p,n): #p ist der punkt n ist der Skalarfaktor
 	if n % 2 == 0 and n != 0: #Wenn der Faktor grade ist kann der erste Schritt gemacht werden, indem der Punkte verdoppelt wird.
 		r = []
@@ -61,9 +62,9 @@ def multiplikation(p,n): #p ist der punkt n ist der Skalarfaktor
 		r.append(Xr)
 		r.append(Yr)
 		r = multiplikation(r,n/2) 
-		"""Hier passiert die Rekrusion, also der Selbstaufruf mit neuen Parametern. Da eine Verdopplung schon stattfand
-		wird das Ergebnis, der neue Punkt, mit der Haelfte des urspruenglichen Faktors in die Funktion gegeben. Waere der Faktor n = 8 wuerde dieser
-		Schritt 3 mal Passieren, da das neue n beim 4. mal 1 ist.""" 
+		#Hier passiert die Rekrusion, also der Selbstaufruf mit neuen Parametern. Da eine Verdopplung schon stattfand
+		#wird das Ergebnis, der neue Punkt, mit der Haelfte des urspruenglichen Faktors in die Funktion gegeben. Waere der Faktor n = 8 wuerde dieser
+		#Schritt 3 mal Passieren, da das neue n beim 4. mal 1 ist.
 		return r
 	if n == 0: #Spezialfall: der Faktor ist 0. 
 		r = []
@@ -72,8 +73,8 @@ def multiplikation(p,n): #p ist der punkt n ist der Skalarfaktor
 		return r
 	if n % 2 == 1 and n != 1:
 		zwischenwert = multiplikation(p,n-1)
-		"""Rekrusion: Beispiel: n = 5. Beim ersten durchlauf kommt er in diese Abfrage und schickt den Punkt mit n = 4 in die Funktion.
-		Darauf wird 2 mal die Verdopplung durchgefuehrt und das Ergebnis mit p addiert: p * 5 = p*2*2 + p."""
+#		Rekrusion: Beispiel: n = 5. Beim ersten durchlauf kommt er in diese Abfrage und schickt den Punkt mit n = 4 in die Funktion.
+#		Darauf wird 2 mal die Verdopplung durchgefuehrt und das Ergebnis mit p addiert: p * 5 = p*2*2 + p.
 		r = additionMG(p,zwischenwert)
 		return r
 	if n == 1: #Hier kommt sie Funktion schliesslich hin bei n = 2^x. Wenn der Faktor schliesslich 1 ist, ist das Erbegnis p.
@@ -98,21 +99,21 @@ def ElGamalDecrypt(cipher,Privatkey): #Mit dem Privatkey und den Cipher wird m e
 	d =  Decimal(zwischenwert.split('u')[1]) #Cipher wird gesplittet
 	c1 = multiplikation(C,Privatkey)[0] #Es wird C mit dem Wert Multipliziert und die X-Koordinate ausgelesen.
 	m1 = str(d/c1 % prim + 1) 
-	"""m wird ermittelt. Da das ergebnis aufgrund der Langen Decimalzahlen bei einer Praezision von 1000 immer sehr knapp unter dem eigentlichen 
-	m (differenz der werte ueblicherweise bei e-900) ist und die Rundenfunktion des flaots zu ungenau ist bei prec = 1000 wird 1 dazu addiert und dann
-	der rest hinter dem Komma, also .9999999... angeschnitten."""
+	#m wird ermittelt. Da das ergebnis aufgrund der Langen Decimalzahlen bei einer Praezision von 1000 immer sehr knapp unter dem eigentlichen 
+	#m (differenz der werte ueblicherweise bei e-900) ist und die Rundenfunktion des flaots zu ungenau ist bei prec = 1000 wird 1 dazu addiert und dann
+	#der rest hinter dem Komma, also .9999999... angeschnitten.
 	m1 = m1.split('.')
 	m1 = int(m1[0])
 	output = UTF8.UTFdeConvert(m1) #die Zahl wird wieder in einen Text gewandelt.
 	return output
 
 def KeyGenerator(password): #generiert das Keypaar aus einem Passwort mit sha3
-	P = [Decimal(1763),Decimal(YCalc(1763))]
+	P = [Decimal(1763),Decimal(YCalc(1763))] #Jan: P0 -> kurze Zahl; P1 -> lange Zahl mit Komma nach ca. 7 Stellen
 	Privat = int(KeyGen.KeyGen(password),16)
-	Public = multiplikation(P,Privat)
+	Public = (multiplikation(P,Privat))
 	return Privat, Public
 
-"""Fuer ein tieferes Verstaendnis, dieser Verschluesselung empfehle ich die theoretische Ausarbeitung zu lesen."""
+#	Fuer ein tieferes Verstaendnis, dieser Verschluesselung empfehle ich die theoretische Ausarbeitung zu lesen.
 
 def handleShellParam(param, default):
 
@@ -128,38 +129,50 @@ def handleShellParam(param, default):
 	return default
 
 task = handleShellParam("t", 0) 
-"""zur Bestimmung was gerade von dem Script verlangt wird, sonst exited er, wenn man verschluesseln will und natuerlich das Password fehlt
-Fuer KeyGen waehlen Sie bitte die 1, fue Encrypt die 2 und fuer Decrypt die 3"""   
+#zur Bestimmung was gerade von dem Script verlangt wird, sonst exited er, wenn man verschluesseln will und natuerlich das Password fehlt
+#Fuer KeyGen waehlen Sie bitte die 1, fue Encrypt die 2 und fuer Decrypt die 3 
 password = handleShellParam("p", 0)
 keyname = handleShellParam("k", 0)
 PlainOrCipher = handleShellParam("poc", 0)
 Key = handleShellParam("key", 0) 
-if task == 1:
+if task == "1":
 	if password != 0 and keyname != 0 and len(password) > 15: #das mit len braucht sha3, sonst bugt das.
 		keys = KeyGenerator(password)
 		privat = keys[0]
 		public = keys[1]
-		NewKey = {"key" : {"name" : keyname, "keys" : {"privkey" : privat, "pubkey" : public}}}
+		print "Private Key: " + str(privat)
+		print "Public Key: " + str(public)
+		#NewKey = {"key" : {"name" : keyname, "keys" : {"privkey" : privat, "pubkey" : public}}}
 	
-		with open("keys.json", 'w') as outfile:
-			json.dump(NewKey, outfile, indent = 3, sort_keys = True)
-			sys.exit(0)
-	elif len(password) < 16:
-		print("Das Passwort ist zu kurz. Die laenge muss mindestens 16 Zeichen betragen")
-		sys.exit(1)
-	elif password = 0:
+		#with open("keys.json", 'w') as outfile:
+			#json.dump(NewKey, outfile, indent = 3, sort_keys = True)
+			#sys.exit(0)
+	elif password == 0:
 		print("Es fehlte das Passwort bei ihrer Eingabe")
 		sys.exit(1)
-	elif keyname = 0:
+	elif len(str(password)) < 16:
+		print("Das Passwort ist zu kurz. Die laenge muss mindestens 16 Zeichen betragen")
+		sys.exit(1)
+	elif keyname == 0:
 		print("Es fehlte der Schluesselname bei ihrer Eingabe")
 		sys.exit(1)
-	else
+	else:
 		print ("Leere Felder mag Deep Thought nicht") #der muss so bleiben!!!
 		sys.exit(1)
-if task == 2:
+if task == "2":
+	Key0 = Key.split(',')[0]
+	Key1 = Key.split(',')[1]
+	Key0 = Key0.split("'")[1]
+	Key0 = Key0.split("'")[0]
+	Key1 = Key1.split("'")[1]
+	Key1 = Key1.split("'")[0]
+	Key = [Decimal(Key0),Decimal(Key1)]
 	print(ElGamal(PlainOrCipher, Key))
 	sys.exit(0)
-if task == 3:
+if task == "3":
+	Key = Decimal(Key)
 	print(ElGamalDecrypt(PlainOrCipher, Key))
 	sys.exit(0)
-#Ja ich weiss, dass evt. manche Kommentare noch geändert werden muessen, aber klappt das so?
+else:
+	print "No task given!"
+#Ja ich weiss, dass evt. manche Kommentare noch geaendert werden muessen, aber klappt das so?
